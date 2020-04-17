e6be5fbcbf72cb3f9437adb8d1bccfa07d95a46b
gucchisk <gucchi_sk@yahoo.co.jp>
2020-04-18 00:24:53 +0900

    Update

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const ward = ['千代田', '中央', '港', '新宿', '文京', '台東', '墨田', '江東', '品川', '目黒', '大田', '世田谷', '渋谷', '中野', '杉並', '豊島',
+	      '北', '荒川', '板橋', '練馬', '足立', '葛飾', '江戸川']
+const city = ['八王子', '立川', '武蔵野', '三鷹', '青梅', '府中', '昭島', '調布', '町田', '小金井', '小平', '日野', '東村山', '国分寺', '国立',
+	      '福生', '狛江', '東大和', '清瀬', '東久留米', '武蔵村山', '多摩', '稲城', '羽村', 'あきる野', '西東京']
+const town = ['瑞穂', '日の出', '奥多摩', '大島', '八丈']
+const village = ['檜原', '利島', '新島', '神津島', '三宅', '御蔵島', '青ヶ島', '小笠原']
+
+function autonomy(name) {
+  if (ward.includes(name)) {
+    return '区'
+  }
+  if (city.includes(name)) {
+    return '市'
+  }
+  if (town.includes(name)) {
+    return '町'
+  }
+  if (village.includes(name)) {
+    return '村'
+  }
+  return ''
+}
+
+function dateString(file) {
+  console.log(new Date(file))
+}
+
 async function getCSV(date) {
   const res = await fetch(`${date}.csv`)
   if (!res.ok) {
@@ -20,7 +47,6 @@ async function getCSV(date) {
 
 async function getDataList() {
   const res = await fetch('latest.txt')
-  console.log(res)
   if (!res.ok) {
     return null
   }
@@ -72,7 +98,8 @@ window.onload = async () => {
       level = level > 5 ? 5 : level
     }
     div.className = `item level${level}`
-    div.innerHTML = `<p class="ward">${obj.ward}</p><p class="num">${obj.num}(${diffText})</p>`
+    const name = obj.ward + autonomy(obj.ward)
+    div.innerHTML = `<p class="ward">${name}</p><p class="num">${obj.num} (${diffText})</p>`
     document.getElementById('grid-main').append(div)
   })
 }
