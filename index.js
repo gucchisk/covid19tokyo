a71c6a28c27f5ba9ea11db52f17e20c15cf05c1b
gucchisk <gucchi_sk@yahoo.co.jp>
2020-05-02 21:20:38 +0900

    Add chart

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
-const ward = ['千代田', '中央', '港', '新宿', '文京', '台東', '墨田', '江東', '品川', '目黒', '大田', '世田谷', '渋谷', '中野', '杉並', '豊島',
-	      '北', '荒川', '板橋', '練馬', '足立', '葛飾', '江戸川']
-const city = ['八王子', '立川', '武蔵野', '三鷹', '青梅', '府中', '昭島', '調布', '町田', '小金井', '小平', '日野', '東村山', '国分寺', '国立',
-	      '福生', '狛江', '東大和', '清瀬', '東久留米', '武蔵村山', '多摩', '稲城', '羽村', 'あきる野', '西東京']
-const town = ['瑞穂', '日の出', '奥多摩', '大島', '八丈']
-const village = ['檜原', '利島', '新島', '神津島', '三宅', '御蔵島', '青ヶ島', '小笠原']
+const wardData = {
+  '千代田': { autonomy: '区', id: 'chiyoda' },
+  '中央': { autonomy: '区', id: 'chuo' },
+  '港': { autonomy: '区', id: 'minato' },
+  '新宿': { autonomy: '区', id: 'shinjuku' },
+  '文京': { autonomy: '区', id: 'bunkyo' },
+  '台東': { autonomy: '区', id: 'taito' },
+  '墨田': { autonomy: '区', id: 'sumida' },
+  '江東': { autonomy: '区', id: 'koto' },
+  '品川': { autonomy: '区', id: 'shinagawa' },
+  '目黒': { autonomy: '区', id: 'meguro' },
+  '大田': { autonomy: '区', id: 'ota' },
+  '世田谷': { autonomy: '区', id: 'setagaya' },
+  '渋谷': { autonomy: '区', id: 'shibuya' },
+  '中野': { autonomy: '区', id: 'nakano' },
+  '杉並': { autonomy: '区', id: 'suginami' },
+  '豊島': { autonomy: '区', id: 'toshima' },
+  '北': { autonomy: '区', id: 'kita' },
+  '荒川': { autonomy: '区', id: 'arakawa' },
+  '板橋': { autonomy: '区', id: 'itabashi' },
+  '練馬': { autonomy: '区', id: 'nerima' },
+  '足立': { autonomy: '区', id: 'adachi' },
+  '葛飾': { autonomy: '区', id: 'katsushika' },
+  '江戸川': { autonomy: '区', id: 'edogawa' },
+  '八王子': { autonomy: '市', id: 'hachioji' },
+  '立川': { autonomy: '市', id: 'tachikawa' },
+  '武蔵野': { autonomy: '市', id: 'musashino' },
+  '三鷹': { autonomy: '市', id: 'mitaka' },
+  '青梅': { autonomy: '市', id: 'ome' },
+  '府中': { autonomy: '市', id: 'fuchu' },
+  '青梅': { autonomy: '市', id: 'ome' },
+  '昭島': { autonomy: '市', id: 'akishima' },
+  '調布': { autonomy: '市', id: 'chofu' },
+  '町田': { autonomy: '市', id: 'machida' },
+  '小金井': { autonomy: '市', id: 'koganei' },
+  '小平': { autonomy: '市', id: 'kodaira' },
+  '日野': { autonomy: '市', id: 'hino' },
+  '東村山': { autonomy: '市', id: 'higashimurayama' },
+  '国分寺': { autonomy: '市', id: 'kokubunji' },
+  '国立': { autonomy: '市', id: 'kunitachi' },
+  '福生': { autonomy: '市', id: 'fussa' },
+  '狛江': { autonomy: '市', id: 'komae' },
+  '東大和': { autonomy: '市', id: 'higashiyamato' },
+  '清瀬': { autonomy: '市', id: 'kiyose' },
+  '東久留米': { autonomy: '市', id: 'higashikurume' },
+  '武蔵村山': { autonomy: '市', id: 'musashimurayama' },
+  '多摩': { autonomy: '市', id: 'tama' },
+  '稲城': { autonomy: '市', id: 'inagi' },
+  '羽村': { autonomy: '市', id: 'hamura' },
+  'あきる野': { autonomy: '市', id: 'akiruno' },
+  '西東京': { autonomy: '市', id: 'nishitokyo' },
+  '瑞穂': { autonomy: '町', id: 'mizuho' },
+  '日の出': { autonomy: '町', id: 'hinode' },
+  '奥多摩': { autonomy: '町', id: 'okutama' },
+  '大島': { autonomy: '町', id: 'oshima' },
+  '八丈': { autonomy: '町', id: 'hachijo' },
+  '檜原': { autonomy: '村', id: 'hinohara' },
+  '利島': { autonomy: '村', id: 'toshimamura' },
+  '新島': { autonomy: '村', id: 'niijima' },
+  '神津島': { autonomy: '村', id: 'kouzushima' },
+  '三宅': { autonomy: '村', id: 'miyake' },  
+  '御蔵島': { autonomy: '村', id: 'mikurasima' },
+  '青ヶ島': { autonomy: '村', id: 'aogashima' },
+  '小笠原': { autonomy: '村', id: 'ogasawara' }
+}
 
 const baseurl = 'https://raw.githubusercontent.com/gucchisk/parse-tokyo-covid-report-pdf'
 const dataurl = 'https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/gh-pages/data/130001_tokyo_covid19_patients.csv'
 
 function autonomy(name) {
-  if (ward.includes(name)) {
-    return '区'
+  const data = wardData[name]
+  if (data === undefined) {
+    return ''
   }
-  if (city.includes(name)) {
-    return '市'
-  }
-  if (town.includes(name)) {
-    return '町'
-  }
-  if (village.includes(name)) {
-    return '村'
-  }
-  return ''
+  return data.autonomy
 }
 
 function dateString(date) {
@@ -74,7 +125,6 @@ async function getDataList() {
 
 async function getCountTokyo(datestr) {
   const dateText = `${datestr.substring(0, 4)}-${datestr.substring(4, 6)}-${datestr.substring(6, 8)}`
-  console.log(dateText)
   const res = await fetch(dataurl)
   if (!res.ok) {
     return null
@@ -106,7 +156,7 @@ window.onload = async () => {
     document.getElementById('date').innerHTML = `${dateString(list[0])} - ${count}<span>人</span>`
   })
   const data = await getData(list)
-  
+
   if (data[0] === null || data[1] === null) {
     error()
     return
@@ -145,7 +195,71 @@ window.onload = async () => {
     }
     div.className = `item level${level}`
     const name = obj.ward + autonomy(obj.ward)
-    div.innerHTML = `<p class="ward">${name}</p><p class="num">${obj.num} (${diffText})</p>`
+    const wdata = wardData[obj.ward]
+    let divtag = '<div>'
+    if (wdata !== undefined) {
+      divtag = `<div id="${wdata.id}" class="clickpop" href="#popup" data-effect="mfp-zoom-in">`
+    }
+    div.innerHTML = `${divtag}<p class="ward">${name}</p><p class="num">${obj.num} (${diffText})</p></div>`
     document.getElementById('grid-main').append(div)
+  })
+
+  $('.clickpop').magnificPopup({
+    removalDelay: 300,
+    callbacks: {
+      beforeOpen: function() {
+	const el = this.st.el
+	const id = el.attr('id')
+	const name = el.children('.ward').text();
+	const filename = `${id}.csv`
+	this.st.mainClass = el.attr('data-effect')
+	fetch(`${baseurl}/master/data/${filename}`).then(res => {
+	  return res.text()
+	}).then(text => {
+	  const lines = text.split('\n')
+	  const x = ['x']
+	  const values = [name]
+	  lines.forEach(line => {
+	    const datevalue = line.split(',')
+	    if (datevalue[1] !== undefined) {
+	      const date = datevalue[0]
+	      x.push(`${date.substring(4, 6)}/${date.substring(6, 8)}`)
+	      values.push(datevalue[1])
+	    }
+	  })
+
+	  const chart = c3.generate({
+	    bindto: '#chart',
+	    data: {
+	      x: 'x',
+	      xFormat: '%m/%d',
+	      columns: [
+		x,
+		values
+	      ],
+	      groups: [
+		['value']
+	      ],
+	      type: 'bar'
+	    },
+	    bar: {
+	      width: {
+		ratio: 0.5
+	      }
+	    },
+	    axis: {
+	      x: {
+		type: 'timeseries',
+		tick: {
+		  culling: true,
+		  format: '%m/%d'
+		}
+	      }
+	    }
+	  })
+	})
+      }
+    },
+    midClick: true
   })
 }
